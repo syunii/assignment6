23af70709b32c327025a91fa5893a7945f9055c1
syunii <yuniww577@gmail.com>
2023-05-15 17:50:39 +0700

    Add new function

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from math_function import add
->
-> def main():
->
->     data_1 = int(input(masukkan input 1 :))
->     data_2 = int(input(masukkan input 2 :))
->     operator = input(masukkan operator :)
->
->     if operator == +:
->         result = add(data_1, data_2)
->
->     print({} {} {} = {} .format(data_1, operator, data_2, result))
->
->
-> if _name_ == _main_:
->     print(Hello Main !)
->     main()
+from math_function import *
+
+
+def main():
+
+    data_1 = int(input("masukkan input 1 : "))
+    data_2 = int(input("masukkan input 2 : "))
+    operator = input("masukkan operator (yang bisa cuma: + , * , /) : ")
+
+    if operator == "+":
+        result = add(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+
+    elif operator == "*":
+        result = multiplication(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+
+    elif operator == "/":
+        result = division(data_1, data_2)
+        print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+
+    else:
+        print("Lo goblog apa gimana?")
+
+
+if __name__ == "__main__":
+    print("Hello Main !")
+    main()
